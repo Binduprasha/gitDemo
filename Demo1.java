bf241ded2c70c5ac9efd3f757c64ae7cdcd2f628
Binduprasha <55555475+Binduprasha@users.noreply.github.com>
2021-05-26 00:16:31 +0530

    version2

--- Demo1.java
+++ Demo1.java
@@ -1,6 +1,6 @@
 public class TestGit{
    public static void main(String args){
-     
-     System.out.println("Hello world");
+     String message="Hello World";
+     System.out.println(message);
    }
 }
