97a56f615740b8478b3acbbbe1b3218c02ff6768
Binduprasha <bindusp4@gmail.com>
2021-05-26 15:20:17 +0530

    Removed merge conflicts

--- TestGit.java
+++ TestGit.java
@@ -1,7 +1,7 @@
 public class TestGit{
-   public static void main(String args){
+   public static void main(String args[]){
+    String message="Hey Hai";
 
-    String message="Hey";
      System.out.println(message);
 
      
